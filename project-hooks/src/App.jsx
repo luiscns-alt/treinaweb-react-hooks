a2f9cef2108018e6dd870be0cf384ac047832351
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 15:48:38 -0300

    feat: comparison with class

---
+++ project-hooks/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component, useState } from 'react';
+import './App.css';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    increment = () => {
+        this.setState(state => ({ counter: state.counter + 1 }));
+    }
+
+    render() {
+        const { state } = this;
+        return (
+            <button onClick={this.increment}>Classe: {state.counter}</button>
+        )
+    }
+}
+
+function Counter2(props) {
+    const [counter, setCounter] = useState(0);
+
+    return (
+        <button onClick={() => setCounter(counter + 1)}>Função: {counter}</button>
+    )
+}
+
+function App() {
+    return (
+        <div className="App">
+            <Counter />
+            <Counter2 />
+        </div>
+    );
+}
+
+export default App;
