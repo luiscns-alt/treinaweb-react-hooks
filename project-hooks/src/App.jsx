16a7df57b359e7c584dd70d16ec05e93f19d41d4
Luis Carlos <luiscarlosnors@gmail.com>
2022-03-27 21:18:48 -0300

    feat: apply useReducer

--- project-hooks/src/App.jsx
+++ project-hooks/src/App.jsx
@@ -3,12 +3,13 @@ import './App.css';
 
 import ThemeProvider from './data/ThemeProvider';
 import Teste from './ui/Teste';
+import Lista from "./ui/Lista";
 
 function App() {
     return (
         <ThemeProvider>
             <div className="App">
-                <Teste />
+                <Lista />
             </div>
         </ThemeProvider>
     );
