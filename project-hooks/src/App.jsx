1d8809619ab0cb51f6e8ef5c087b226bafe2e197
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-09 20:19:05 -0300

    hook: Pegando um valor anterior (funcionalidade de desfazer)

--- project-hooks/src/App.jsx
+++ project-hooks/src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 
 import ThemeProvider from './data/ThemeProvider';
 import useLocalStorage from './hooks/seLocalStorage';
+import useUndo from './hooks/useUndo';
 
 function App() {
-    const [counter, setCounter] = useLocalStorage('counter', 1);
+    const [counter, setCounter] = useState(0);
+    const prevValue = useUndo(counter);
 
     return (
         <ThemeProvider>
             <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+            <div>Valor Anterior: {prevValue}</div>
+            <button onClick={() => setCounter(prevValue)}>Voltar</button>
         </ThemeProvider>
     );
 }
