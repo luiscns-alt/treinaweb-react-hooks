95db0aee7c9c759fdb2f1e01b102b140e0ce0c4d
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-09 20:31:21 -0300

    Testando Hooks : Conheça o useDebugValue

---
+++ project-hooks/src/hooks/useWindowSize.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect, useDebugValue } from 'react';
+
+function getWindowSize() {
+    return {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    };
+}
+
+export default function useWindowSize() {
+    const [size, setSize] = useState(getWindowSize());
+
+    function updateSize() {
+        setSize(getWindowSize());
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', updateSize);
+        return () => {
+            window.removeEventListener('resize', updateSize);
+        };
+    }, []);
+
+    useDebugValue(
+        size.height < 120 ? 'Janela Pequena' : 'Janela Grande',
+        (text) => {
+            return text.toUpperCase();
+        }
+    );
+
+    return size;
+}
