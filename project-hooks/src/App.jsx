a77b0d05c82a151f1de0aaf398d2849b0a2a89a8
Luis Carlos <luiscarlosnors@gmail.com>
2022-04-09 19:52:55 -0300

    cacheando funções com useCallback

--- project-hooks/src/App.jsx
+++ project-hooks/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css';
 
 import ThemeProvider from './data/ThemeProvider';
 import Teste from './ui/Teste';
-import Lista from "./ui/Lista";
+import Lista from './ui/Lista';
+import ExemploUseCallback from './ui/ExemploUseCallback';
 
 function App() {
     return (
         <ThemeProvider>
-            <div className="App">
-                <Lista />
+            <div className='App'>
+                <ExemploUseCallback />
             </div>
         </ThemeProvider>
     );
